939a6e5686e4e0a7c4203bf1da9062263169cc05
Danil Fartnov <fartanovda@gmail.com>
2023-05-16 14:35:35 +0300

    divided scraper classes

---
+++ job_posting.py
@@ -0,0 +1,11 @@
+class JobPosting:
+    """
+    Have all the job necessary attributes
+    """
+    def __init__(self, title, experience, company, location, link, salary=None):
+        self.title = title
+        self.experience = experience
+        self.company = company
+        self.location = location
+        self.salary = salary
+        self.link = link
